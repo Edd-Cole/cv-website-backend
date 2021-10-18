4864591ec514d18f27876ddc4d01998a2443f485
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 13:02:08 +0100

    correcting test, now passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,23 +1,22 @@
-const app = require("../app.js");
-    const request = require("supertest");
-    const db = require("../database/connection/connection.js");
-    const data = require("../database/data/testData/exports.js);
-    const seed = require("../database/seed/seed.js");
-    const fs = require("fs/promises");
+const app = require("../routers/app.js");
+const request = require("supertest");
+const { db, client } = require("../database/connection/connection.js");
+const data = require("../database/data/testData/exports.js");
+const seed = require("../database/seed/seed.js");
+const fs = require("fs/promises");
 
-    beforeEach(() => seed(data));
-    afterAll(() => )
+beforeEach(() => seed(data));
+afterAll(() => db().then(() => client.close()))
 
-    describe("cv-site /api", () => {
-        describe("/ - GET", () => {
-            test("Returns all available endpoints within the api", () => {
-                return request(app).get("/api").expect(200).then(async ({ body }) => {
-                    const endpoints = await fs.readFile(`${__dirname}/../endpoints.json`)
-                    .then(ep => ep.json());
-                    expect(typeof(body.endpoints)).toBe("object");
-                    expect(body.endpoints).toEqual(endpoints);
-                })
+describe("cv-site /api", () => {
+    describe("/ - GET", () => {
+        test("Returns all available endpoints within the api", () => {
+            return request(app).get("/api").expect(200).then(async({ body }) => {
+                const endpoints = await fs.readFile(`${__dirname}/../endpoints.json`)
+                    .then(endpoints => JSON.parse(endpoints))
+                expect(typeof(body.endpoints)).toBe(typeof(endpoints));
+                expect(body.endpoints).toEqual(endpoints);
             })
-        }
+        })
     })
-    
+})
