ea1015e404b66ee0af3f6b2b2ab2acf8af277c45
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 13:23:56 +0100

    Adding documentation

--- routers/controllers/articles.controller.js
+++ routers/controllers/articles.controller.js
@@ -1,11 +1,14 @@
 const { fetchArticles } = require("../models/articles.model.js");
 
 const getArticles = (request, response, next) => {
+    //Invoke the model to get the articles from the database
     return fetchArticles()
         .then(articles => {
+            //If successful, return the articles
             response.status(200).send({ articles });
         })
         .catch(error => {
+            //Otherwise, send an error
             next(error);
         })
 }
--- routers/routes/apiArticles.js
+++ routers/routes/apiArticles.js
@@ -1,6 +1,7 @@
 const apiArticles = require("express").Router();
 const { getArticles } = require("../controllers/articles.controller.js");
 
+//Route /api/articles - GET, POST
 apiArticles.route("/").get(getArticles)
 
 module.exports = apiArticles;
