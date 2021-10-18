d0fb1e2d90fdcbc15d9859d9aee7b2e0fe024f0a
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 13:10:35 +0100

    New test for endpoint /api/articles - GET

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,12 +10,37 @@ afterAll(() => db().then(() => client.close()))
 
 describe("cv-site /api", () => {
     describe("/ - GET", () => {
-        test("Returns all available endpoints within the api", () => {
-            return request(app).get("/api").expect(200).then(async({ body }) => {
-                const endpoints = await fs.readFile(`${__dirname}/../endpoints.json`)
-                    .then(endpoints => JSON.parse(endpoints))
-                expect(typeof(body.endpoints)).toBe(typeof(endpoints));
-                expect(body.endpoints).toEqual(endpoints);
+        describe("status 200 - Success", () => {
+            test("Returns all available endpoints within the api", () => {
+                return request(app).get("/api").expect(200).then(async({ body }) => {
+                    const endpoints = await fs.readFile(`${__dirname}/../endpoints.json`)
+                        .then(endpoints => JSON.parse(endpoints))
+                    expect(typeof(body.endpoints)).toBe(typeof(endpoints));
+                    expect(body.endpoints).toEqual(endpoints);
+                })
+            })
+        })
+    })
+
+    describe("/articles", () => {
+        describe("/ - GET", () => {
+            describe("status 200 - Success", () => {
+                test("Returns an array of all the articles in the database", () => {
+                    return request(app)
+                        .get("/api/articles")
+                        .expect(200)
+                        .then(response => {
+                            expect(response.body.articles.length).toBeGreaterThan(0);
+                            response.body.articles.forEach(article => {
+                                expect(article).toMatchObject({
+                                    author: expect.any(String),
+                                    date: expect.any(String),
+                                    title: expect.any(String),
+                                    article: expect.any(String)
+                                })
+                            })
+                        })
+                })
             })
         })
     })
