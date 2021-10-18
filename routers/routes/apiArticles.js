07b3c8b7a22da55e29f91b17a3a054604eb8fb2c
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 13:34:18 +0100

    Updating routes with documentation

--- routers/routes/apiArticles.js
+++ routers/routes/apiArticles.js
@@ -4,4 +4,7 @@ const { getArticles } = require("../controllers/articles.controller.js");
 //Route /api/articles - GET, POST
 apiArticles.route("/").get(getArticles)
 
+//Route /api/articles/:title - GET
+apiArticles.route("/:title")
+
 module.exports = apiArticles;
