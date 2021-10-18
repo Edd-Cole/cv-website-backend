0d3b4150235723f372b7b440e4ee79049ffaf111
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 11:42:42 +0100

    creating seed function

--- database/seed/seed.js
+++ database/seed/seed.js
@@ -2,7 +2,13 @@ const db = require("../seed/connection.js");
 const {} = require("../../utils/utils.js");
 
 async function seed(data) {
-
+    return db()
+        .then(async(db) => {
+            await db.collection("articles").drop();
+            await db.createCollection("articles")
+            await db.collection("articles").insertMany(data.articleData)
+            return db
+        })
 }
 
 module.exports = seed;
