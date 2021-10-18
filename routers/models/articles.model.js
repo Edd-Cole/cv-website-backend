3bee6569cb7e1f477fc9aaf3e428bd0ba473ad1a
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 13:21:41 +0100

    Adding documentation

--- routers/models/articles.model.js
+++ routers/models/articles.model.js
@@ -1,14 +1,17 @@
 const { db } = require("../../database/connection/connection.js");
 
 const fetchArticles = () => {
+    //Connect to DB
     return db()
         .then(db => {
+            //Find and return the relevant articles in reverse chronological order
             return db.collection("articles")
                 .find({})
                 .sort({ date: -1 })
                 .toArray()
         })
         .then(articles => {
+            //Remove any id tags and the return to the controller
             articles = articles.map(article => {
                 delete article._id;
                 return article;
