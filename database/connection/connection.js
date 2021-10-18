f1f98231e683e603c20f267eea4c9642b73c78ee
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 11:42:32 +0100

    Updating connections to work

--- database/connection/connection.js
+++ database/connection/connection.js
@@ -1,24 +1,23 @@
 const { MongoClient } = require("mongodb");
 
-    const url = "mongodb://localhost:27017";
-    let client = new MongoClient(url);
-    
-    const ENV = process.env.NODE_ENV || "development";
-    
-    let dbSuffix = process.env.NODE_ENV || "";
-    
-    const dbName = ENV = "test" ? "cv-site-test" : "cv-site-dev";
-    
-    if(ENV === "production") {
-        const uri = //Add in on hosting
+const url = "mongodb://localhost:27017";
+let client = new MongoClient(url);
+
+const ENV = process.env.NODE_ENV || "development";
+
+let dbSuffix = process.env.NODE_ENV || "";
+
+const dbName = ENV === "test" ? "cv-site-test" : "cv-site-dev";
+
+if (ENV === "production") {
+    const uri = //Add in on hosting
         client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    }
-    
-    const db = async () => {
-        await client.connect();
-        const db = client.db(dbName);
-        return db;
-    };
-    
-    module.exports = { client, db };    
-    
+}
+
+const db = async() => {
+    await client.connect();
+    const db = client.db(dbName);
+    return db;
+};
+
+module.exports = { client, db };
--- database/connection/initialiseDB.js
+++ database/connection/initialiseDB.js
@@ -2,8 +2,12 @@ const { client } = require('./connection');
 
 const initialiseDB = async(name) => {
     await client.connect();
-    const db1 = await client.db(`${name}-backend`);
-    const db2 = await client.db(`${name}-frontend`);
+    const db1 = await client.db(`${name}-test`);
+    const db2 = await client.db(`${name}-dev`);
+    await db1.createCollection("articles");
+    await db2.createCollection("articles");
+    console.log("db's initialised")
+    return client.close();
 }
 
-initialiseDB();
+initialiseDB("cv-website");
