b40bf28db3cdf48156dfacfeb0197fe2f0c71537
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-18 11:49:24 +0100

    New test data

--- database/data/testData/articleData.js
+++ database/data/testData/articleData.js
@@ -1,1 +1,19 @@
-export const articleData = []
+export const articleData = [{
+        author: "Edd Kleszcz",
+        date: new Date(),
+        title: "test",
+        article: "1 2 3 4 5 6 7 8 9 10, HELLO!"
+    },
+    {
+        author: "Billy Bob",
+        date: new Date(),
+        title: "James Bond",
+        article: "I love Bond, I have every film ever made"
+    },
+    {
+        author: "Man in Black",
+        date: new Date(),
+        title: "I'm Lost",
+        article: "Do you get the joke?"
+    }
+]
