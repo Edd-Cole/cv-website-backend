514916a6667d40a307347a68cf934066558a027a
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-20 11:12:58 +0100

    Updating test data to include varying dates

--- database/data/testData/articleData.js
+++ database/data/testData/articleData.js
@@ -1,18 +1,18 @@
 const articleData = [{
         author: "Edd Kleszcz",
-        date: new Date(),
+        date: new Date(1000000),
         title: "test",
         article: "1 2 3 4 5 6 7 8 9 10, HELLO!"
     },
     {
         author: "Billy Bob",
-        date: new Date(),
+        date: new Date(10),
         title: "James Bond",
         article: "I love Bond, I have every film ever made"
     },
     {
         author: "Man in Black",
-        date: new Date(),
+        date: new Date(10000000000),
         title: "I'm Lost",
         article: "Do you get the joke?"
     }
